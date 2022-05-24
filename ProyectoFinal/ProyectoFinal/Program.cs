0937bf2d8511f8ba4da7ef10627b8744cc27a10b
josueMerino <josue.merino@alumnos.uneatlantico.es>
2022-05-25 01:17:04 +0200

    First commit

---
+++ ProyectoFinal/ProyectoFinal/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Xml;
+
+namespace ProyectoFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TextoEncriptado informacion = TextoEncriptado.getTexto();
+
+            //Algoritmo textoResuelto = Algoritmo.desencriptar(informacion);
+
+            //Console.WriteLine(textoResuelto);
+
+            StreamReader file = new StreamReader("../../../informacion.txt");
+            string fileContent = file.ReadToEnd();
+            file.Close();
+            byte[] text = Encoding.UTF8.GetBytes(fileContent);
+            
+            XmlDocument claves = new XmlDocument();
+            claves.Load("../../../claves.xml");
+          
+            XmlNode node = claves.DocumentElement.SelectSingleNode("/ALGORITMO/clavePrivada");
+            String clavePrivada = node.InnerText;
+
+            XmlDocument clavePrivadas = new XmlDocument();
+            clavePrivadas.LoadXml(clavePrivada);
+
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(1024);
+
+            rsa.FromXmlString(clavePrivada);
+      
+
+            byte[] decryptedMessageBytes = rsa.Decrypt(text, true);
+            string decryptedMessageString = Encoding.UTF8.GetString(decryptedMessageBytes);
+            Console.WriteLine(decryptedMessageString.ToString());
+            
+
+        }
+    }
+}
---
+++ ProyectoFinal/ProyectoFinal/TextoEncriptado.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace ProyectoFinal
+{
+    public class TextoEncriptado
+    {
+        private static StreamReader rutaHaciaTexto = new StreamReader("../../../../informacion.txt");
+
+
+
+    }
+}
