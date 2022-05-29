adefe269e874447cefea5025ebdd0a1e8762a0cf
josueMerino <josue.merino@alumnos.uneatlantico.es>
2022-05-29 22:35:40 +0200

    Creando clases de gestión de Claves y de Mensaje

---
+++ ProyectoFinal/CriptoFinalForm/Clave.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CriptoFinalForm
+{
+    public class Clave
+    {
+        private XmlNode claves;
+
+        public Clave(XmlDocument claves)
+        {
+            this.claves = this.getClavesFromXml(claves);
+        }
+
+        public String getClavePublica()
+        {
+            return this.claves.SelectSingleNode("/clave").InnerText;
+        }
+
+        public String getClavePrivada()
+        {
+            return this.claves.SelectSingleNode("/clavePrivada").InnerText;
+        }
+
+        private XmlNode getClavesFromXml(XmlDocument claves)
+        {
+            return claves.DocumentElement.SelectSingleNode("/ALGORITMO");
+        }
+    }
+}
---
+++ ProyectoFinal/CriptoFinalForm/Mensaje.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriptoFinalForm
+{
+    public class Mensaje
+    {
+        private String texto;
+
+        public Mensaje(string texto)
+        {
+            this.texto = texto;
+        }
+
+        public byte[] getMensajeParaDesencriptar()
+        {
+            return Convert.FromBase64String(Encoding.UTF8.GetString(Convert.FromBase64String(this.texto))); ;
+        }
+    }
+}
